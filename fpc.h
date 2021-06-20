1a58749e94dc5aa23185c780faeb945d6e78d8a7
MarcelPiNacy <marcelpi97@gmail.com>
2021-06-20 16:30:14 +0200

    Minor changes.

--- fpc.h
+++ fpc.h
@@ -100,12 +100,12 @@ FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
     fpc_context_t* ctx,
     const double* values,
     size_t value_count,
-    uint8_t* out_headers,
+    void* out_headers,
     void* out_compressed);
 
 FPC_ATTR void FPC_CALL fpc_decode_explicit(
     fpc_context_t* ctx,
-    const uint8_t* headers,
+    const void* headers,
     const void* compressed,
     double* out_values,
     size_t out_count);
@@ -114,11 +114,11 @@ FPC_ATTR size_t FPC_CALL fpc_encode(
     fpc_context_t* ctx,
     const double* values,
     size_t value_count,
-    uint8_t* out_compressed);
+    void* out_compressed);
 
 FPC_ATTR void FPC_CALL fpc_decode(
     fpc_context_t* ctx,
-    const uint8_t* compressed,
+    const void* compressed,
     double* out_values,
     size_t out_count);
 
@@ -126,12 +126,12 @@ FPC_ATTR size_t FPC_CALL fpc32_encode_explicit(
     fpc32_context_t* ctx,
     const float* values,
     size_t value_count,
-    uint8_t* out_headers,
+    void* out_headers,
     void* out_compressed);
 
 FPC_ATTR void FPC_CALL fpc32_decode_explicit(
     fpc32_context_t* ctx,
-    const uint8_t* headers,
+    const void* headers,
     const void* compressed,
     float* out_values,
     size_t out_count);
@@ -140,11 +140,11 @@ FPC_ATTR size_t FPC_CALL fpc32_encode(
     fpc32_context_t* ctx,
     const float* values,
     size_t value_count,
-    uint8_t* out_compressed);
+    void* out_compressed);
 
 FPC_ATTR void FPC_CALL fpc32_decode(
     fpc32_context_t* ctx,
-    const uint8_t* compressed,
+    const void* compressed,
     float* out_values,
     size_t out_count);
 FPC_EXTERN_C_END
@@ -190,20 +190,65 @@ FPC_EXTERN_C_END
 #endif
 #endif
 
-#include <string.h>
-
 #define FPC_FCM_HASH_UPDATE(HASH, VALUE)   HASH = ((HASH << ctx->hash_args.fcm_lshift) ^ (size_t)((VALUE) >> ctx->hash_args.fcm_rshift)) & fcm_mod_mask
 #define FPC_DFCM_HASH_UPDATE(HASH, VALUE)  HASH = ((HASH << ctx->hash_args.dfcm_lshift) ^ (size_t)((VALUE) >> ctx->hash_args.dfcm_rshift)) & dfcm_mod_mask
+
+#define FPC_IS_ALIGNED(PTR, ALIGN) (((size_t)(PTR) & (size_t)((ALIGN) - 1)) == 0)
+
+#include <string.h>
+
+#define FPC_COPY_FLOAT2(OUT, IN)            \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 4));   \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 8));  \
+    (void)memcpy((OUT), (IN), 8)
+
+#define FPC_COPY_DOUBLE2(OUT, IN)           \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 8));   \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 16)); \
+    (void)memcpy((OUT), (IN), 16)
+
+#define FPC_COPY_FLOAT(OUT, IN)             \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 4));   \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 4));  \
+    (void)memcpy((OUT), (IN), 4)
+
+#define FPC_COPY_DOUBLE(OUT, IN)            \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 8));   \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 8));  \
+    (void)memcpy((OUT), (IN), 8)
+
+#define FPC_LOAD_COMPRESSED_DOUBLE(OUT, IN, COUNT)  \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 8));          \
+    FPC_INVARIANT((COUNT) >= 0 && (COUNT) <= 8);    \
+    (void)memcpy((OUT), (IN), (COUNT))
+
+#define FPC_LOAD_COMPRESSED_FLOAT(OUT, IN, COUNT)   \
+    FPC_INVARIANT(FPC_IS_ALIGNED(OUT, 4));          \
+    FPC_INVARIANT((COUNT) >= 0 && (COUNT) <= 4);    \
+    (void)memcpy((OUT), (IN), (COUNT))
+
+#define FPC_STORE_COMPRESSED_DOUBLE(OUT, IN, COUNT) \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 8));           \
+    FPC_INVARIANT((COUNT) >= 0 && (COUNT) <= 8);    \
+    (void)memcpy((OUT), (IN), (COUNT))
+
+#define FPC_STORE_COMPRESSED_FLOAT(OUT, IN, COUNT)  \
+    FPC_INVARIANT(FPC_IS_ALIGNED(IN, 4));           \
+    FPC_INVARIANT((COUNT) >= 0 && (COUNT) <= 4);    \
+    (void)memcpy((OUT), (IN), (COUNT))
 
 FPC_EXTERN_C_BEGIN
 FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
     fpc_context_t* ctx,
-    const double* values, size_t value_count,
-    uint8_t* out_headers, void* out_compressed)
+    const double* values,
+    size_t value_count,
+    void* out_headers,
+    void* out_compressed)
 {
     const double* end;
     uint8_t* compressed_begin;
     uint8_t* out;
+    uint8_t* out_h;
     uint8_t type_a, type_b, lzbc_a, lzbc_b;
     size_t fcm_mod_mask, dfcm_mod_mask, fcm_hash, dfcm_hash;
     uint64_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value, fcm_xor, dfcm_xor;
@@ -213,6 +258,7 @@ FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
     FPC_INVARIANT(
         (uint8_t*)out_headers < (uint8_t*)out_compressed ||
         (uint8_t*)out_headers + value_count >= (uint8_t*)out_compressed);
+    out_h = (uint8_t*)out_headers;
     end = values + value_count;
     compressed_begin = out = (uint8_t*)out_compressed;
     prior_value = *(const uint64_t*)&ctx->seed;
@@ -221,7 +267,7 @@ FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
     dfcm_mod_mask = ctx->dfcm_size - 1;
     while ((end - values) >= 2)
     {
-        (void)memcpy(tmp, values, 16);
+        FPC_COPY_DOUBLE2(tmp, values);
         values += 2;
         // FCM STEP #1
         fcm_xor = tmp[0] ^ fcm_prediction;
@@ -258,20 +304,20 @@ FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
         lzbc_b = FPC_CLZ64(tmp[1]) >> 3;
         lzbc_a -= (lzbc_a == FPC_LEAST_FREQUENT_LZBC);
         lzbc_b -= (lzbc_b == FPC_LEAST_FREQUENT_LZBC);
-        *out_headers =
+        *out_h =
             ((type_b | (lzbc_b - (lzbc_b > FPC_LEAST_FREQUENT_LZBC))) << 4) |
             (type_a | (lzbc_a - (lzbc_a > FPC_LEAST_FREQUENT_LZBC)));
-        ++out_headers;
+        ++out_h;
         lzbc_a = 8 - lzbc_a;
         lzbc_b = 8 - lzbc_b;
-        (void)memcpy(out, tmp, lzbc_a);
+        FPC_STORE_COMPRESSED_DOUBLE(out, tmp, lzbc_a);
         out += lzbc_a;
-        (void)memcpy(out, tmp + 1, lzbc_b);
+        FPC_STORE_COMPRESSED_DOUBLE(out, tmp + 1, lzbc_b);
         out += lzbc_b;
     }
     if (values != end)
     {
-        (void)memcpy(tmp, values, 8);
+        FPC_COPY_DOUBLE(tmp, values);
         // LAST FCM STEP
         fcm_xor = tmp[0] ^ fcm_prediction;
         // LAST DFCM STEP
@@ -282,25 +328,31 @@ FPC_ATTR size_t FPC_CALL fpc_encode_explicit(
         // COMPRESS LAST VALUE
         lzbc_a = FPC_CLZ64(tmp[0]) >> 3;
         lzbc_a -= (lzbc_a == FPC_LEAST_FREQUENT_LZBC);
-        *out_headers = (type_a << 3) | (lzbc_a - (lzbc_a > FPC_LEAST_FREQUENT_LZBC));
+        *out_h = (type_a << 3) | (lzbc_a - (lzbc_a > FPC_LEAST_FREQUENT_LZBC));
         lzbc_a = 8 - lzbc_a;
-        (void)memcpy(out, tmp, lzbc_a);
+        FPC_STORE_COMPRESSED_DOUBLE(out, tmp, lzbc_a);
+        out += lzbc_a;
     }
     return (out - compressed_begin) + (value_count + 1) / 2;
 }
 
 FPC_ATTR void FPC_CALL fpc_decode_explicit(
     fpc_context_t* ctx,
-    const uint8_t* headers,
+    const void* headers,
     const void* compressed,
-    double* out_values, size_t out_count)
+    double* out_values,
+    size_t out_count)
 {
     uint8_t* in;
+    uint8_t* in_headers;
     double* end;
     uint8_t lzbc, header;
     size_t fcm_mod_mask, dfcm_mod_mask, fcm_hash, dfcm_hash;
-    uint64_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value, tmp[2];
+    uint64_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value;
+    FPC_ALIGN(16)
+    uint64_t tmp[2];
     in = (uint8_t*)compressed;
+    in_headers = (uint8_t*)headers;
     end = out_values + out_count;
     fcm_mod_mask = ctx->fcm_size - 1;
     dfcm_mod_mask = ctx->dfcm_size - 1;
@@ -310,15 +362,15 @@ FPC_ATTR void FPC_CALL fpc_decode_explicit(
     while (out_values != end)
     {
         // READ HEADER PAIR
-        header = *headers;
-        ++headers;
+        header = *in_headers;
+        ++in_headers;
         // DECODE HEADER #1
         lzbc = (header & 7);
         lzbc += (lzbc >= FPC_LEAST_FREQUENT_LZBC);
         lzbc = 8 - lzbc;
         // READ COMPRESSED VALUE #1
         tmp[0] = 0;
-        (void)memcpy(tmp, in, lzbc);
+        FPC_LOAD_COMPRESSED_DOUBLE(tmp, in, lzbc);
         in += lzbc;
         // DECODE VALUE #1
         tmp[0] ^= (header & 8) ? dfcm_prediction : fcm_prediction;
@@ -335,7 +387,7 @@ FPC_ATTR void FPC_CALL fpc_decode_explicit(
         ++out_values;
         if (out_values == end)
         {
-            (void)memcpy(out_values, tmp, 8);
+            FPC_COPY_DOUBLE(out_values, tmp);
             return;
         }
         // DECODE HEADER #2
@@ -345,7 +397,7 @@ FPC_ATTR void FPC_CALL fpc_decode_explicit(
         lzbc = 8 - lzbc;
         // READ COMPRESSED VALUE #2
         tmp[1] = 0;
-        (void)memcpy(tmp + 1, in, lzbc);
+        FPC_LOAD_COMPRESSED_DOUBLE(tmp + 1, in, lzbc);
         in += lzbc;
         // DECODE VALUE #2
         tmp[1] ^= (header & 8) ? dfcm_prediction : fcm_prediction;
@@ -360,53 +412,59 @@ FPC_ATTR void FPC_CALL fpc_decode_explicit(
         dfcm_prediction = ctx->dfcm[dfcm_hash] + tmp[1];
         prior_value = tmp[1];
         // STORE DECOMPRESSED PAIR
-        (void)memcpy(out_values - 1, tmp, 16);
+        FPC_COPY_DOUBLE2(out_values - 1, tmp);
         ++out_values;
     }
 }
 
 FPC_ATTR size_t FPC_CALL fpc_encode(
     fpc_context_t* ctx,
-    const double* values, size_t value_count,
-    uint8_t* out_compressed)
+    const double* values,
+    size_t value_count,
+    void* out_compressed)
 {
-    return fpc_encode_explicit(ctx, values, value_count, out_compressed, out_compressed + FPC_UPPER_BOUND_METADATA(value_count));
+    return fpc_encode_explicit(ctx, values, value_count, out_compressed, (uint8_t*)out_compressed + FPC_UPPER_BOUND_METADATA(value_count));
 }
 
 FPC_ATTR void FPC_CALL fpc_decode(
     fpc_context_t* ctx,
-    const uint8_t* compressed,
-    double* out_values, size_t out_count)
+    const void* compressed,
+    double* out_values,
+    size_t out_count)
 {
-    fpc_decode_explicit(ctx, compressed, compressed + FPC_UPPER_BOUND_METADATA(out_count), out_values, out_count);
+    fpc_decode_explicit(ctx, compressed, (uint8_t*)compressed + FPC_UPPER_BOUND_METADATA(out_count), out_values, out_count);
 }
 
 FPC_ATTR size_t FPC_CALL fpc32_encode_explicit(
     fpc32_context_t* ctx,
     const float* values,
     size_t value_count,
-    uint8_t* out_headers,
+    void* out_headers,
     void* out_compressed)
 {
     const float* end;
-    uint8_t* compressed_begin;
     uint8_t* out;
+    uint8_t* out_h;
     uint8_t type_a, type_b, lzbc_a, lzbc_b;
     size_t fcm_mod_mask, dfcm_mod_mask, fcm_hash, dfcm_hash;
-    uint32_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value, fcm_xor, dfcm_xor, tmp[2];
+    uint32_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value, fcm_xor, dfcm_xor;
+    FPC_ALIGN(8)
+    uint32_t tmp[2];
     FPC_INVARIANT((uint8_t*)out_headers != (uint8_t*)out_compressed);
     FPC_INVARIANT(
         (uint8_t*)out_headers < (uint8_t*)out_compressed ||
         (uint8_t*)out_headers + value_count >= (uint8_t*)out_compressed);
     end = values + value_count;
-    compressed_begin = out = (uint8_t*)out_compressed;
+    out = (uint8_t*)out_compressed;
+    out_h = (uint8_t*)out_headers;
     prior_value = *(const uint32_t*)&ctx->seed;
-    fcm_prediction = dfcm_prediction = fcm_hash = dfcm_hash = 0;
+    fcm_prediction = dfcm_prediction = 0;
+    fcm_hash = dfcm_hash = 0;
     fcm_mod_mask = ctx->fcm_size - 1;
     dfcm_mod_mask = ctx->dfcm_size - 1;
     while ((end - values) >= 2)
     {
-        (void)memcpy(tmp, values, 8);
+        FPC_COPY_FLOAT2(tmp, values);
         values += 2;
         // FCM STEP #1
         fcm_xor = tmp[0] ^ fcm_prediction;
@@ -422,7 +480,6 @@ FPC_ATTR size_t FPC_CALL fpc32_encode_explicit(
         dfcm_prediction = ctx->dfcm[dfcm_hash];
         type_a = fcm_xor > dfcm_xor;
         tmp[0] = type_a ? dfcm_xor : fcm_xor;
-        type_a <<= 3;
         // FCM STEP #2
         fcm_xor = tmp[1] ^ fcm_prediction;
         ctx->fcm[fcm_hash] = tmp[1];
@@ -437,22 +494,23 @@ FPC_ATTR size_t FPC_CALL fpc32_encode_explicit(
         dfcm_prediction = ctx->dfcm[dfcm_hash];
         type_b = fcm_xor > dfcm_xor;
         tmp[1] = type_b ? dfcm_xor : fcm_xor;
-        type_b <<= 3;
         // COMPRESS PAIR OF VALUES
         lzbc_a = FPC_CLZ32(tmp[0]) >> 3;
         lzbc_b = FPC_CLZ32(tmp[1]) >> 3;
-        *out_headers = ((type_b | lzbc_b) << 4) | (type_a | lzbc_a);
-        ++out_headers;
+        type_a <<= 3;
+        type_b <<= 3;
+        *out_h = ((type_b | lzbc_b) << 4) | (type_a | lzbc_a);
+        ++out_h;
         lzbc_a = 4 - lzbc_a;
         lzbc_b = 4 - lzbc_b;
-        (void)memcpy(out, tmp, lzbc_a);
+        FPC_STORE_COMPRESSED_FLOAT(out, tmp, lzbc_a);
         out += lzbc_a;
-        (void)memcpy(out, tmp + 1, lzbc_b);
+        FPC_STORE_COMPRESSED_FLOAT(out, tmp + 1, lzbc_b);
         out += lzbc_b;
     }
     while (values != end)
     {
-        (void)memcpy(tmp, values, 4);
+        FPC_COPY_FLOAT(tmp, values);
         // LAST FCM STEP
         fcm_xor = tmp[0] ^ fcm_prediction;
         // LAST DFCM STEP
@@ -463,43 +521,48 @@ FPC_ATTR size_t FPC_CALL fpc32_encode_explicit(
         // COMPRESS LAST VALUE
         lzbc_a = FPC_CLZ32(tmp[0]) >> 3;
         lzbc_a -= (lzbc_a == FPC_LEAST_FREQUENT_LZBC);
-        *out_headers = (type_a << 3) | (lzbc_a - (lzbc_a > FPC_LEAST_FREQUENT_LZBC));
+        *out_h = (type_a << 3) | (lzbc_a - (lzbc_a > FPC_LEAST_FREQUENT_LZBC));
         lzbc_a = 4 - lzbc_a;
-        (void)memcpy(out, tmp, lzbc_a);
+        FPC_STORE_COMPRESSED_FLOAT(out, tmp, lzbc_a);
+        out += lzbc_a;
     }
-    return (out - compressed_begin) + (value_count + 1) / 2;
+    return (out - (uint8_t*)out_compressed) + (value_count + 1) / 2;
 }
 
 FPC_ATTR void FPC_CALL fpc32_decode_explicit(
     fpc32_context_t* ctx,
-    const uint8_t* headers,
+    const void* headers,
     const void* compressed,
     float* out_values,
     size_t out_count)
 {
     uint8_t* in;
+    uint8_t* in_headers;
     float* end;
     uint8_t lzbc, header;
     size_t fcm_mod_mask, dfcm_mod_mask, fcm_hash, dfcm_hash;
-    uint32_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value, tmp[2];
+    uint32_t fcm_prediction, dfcm_prediction, dfcm_delta, prior_value;
+    FPC_ALIGN(8)
+    uint32_t tmp[2];
     in = (uint8_t*)compressed;
+    in_headers = (uint8_t*)headers;
     end = out_values + out_count;
     fcm_mod_mask = ctx->fcm_size - 1;
     dfcm_mod_mask = ctx->dfcm_size - 1;
-    (void)memcpy(&prior_value, &ctx->seed, 4);
+    FPC_COPY_FLOAT(&prior_value, &ctx->seed);
     fcm_hash = dfcm_hash = 0;
     fcm_prediction = dfcm_prediction = 0;
     while (out_values != end)
     {
         // READ HEADER PAIR
-        header = *headers;
-        ++headers;
+        header = *in_headers;
+        ++in_headers;
         // DECODE HEADER #1
         lzbc = (header & 7);
         lzbc = 4 - lzbc;
         // READ COMPRESSED VALUE #1
         tmp[0] = 0;
-        (void)memcpy(tmp, in, lzbc);
+        FPC_LOAD_COMPRESSED_FLOAT(tmp, in, lzbc);
         in += lzbc;
         // DECODE VALUE #1
         tmp[0] ^= (header & 8) ? dfcm_prediction : fcm_prediction;
@@ -516,7 +579,7 @@ FPC_ATTR void FPC_CALL fpc32_decode_explicit(
         ++out_values;
         if (out_values == end)
         {
-            (void)memcpy(out_values, tmp, 4);
+            FPC_COPY_FLOAT(out_values, tmp);
             return;
         }
         // DECODE HEADER #2
@@ -525,7 +588,7 @@ FPC_ATTR void FPC_CALL fpc32_decode_explicit(
         lzbc = 4 - lzbc;
         // READ COMPRESSED VALUE #2
         tmp[1] = 0;
-        (void)memcpy(tmp + 1, in, lzbc);
+        FPC_LOAD_COMPRESSED_FLOAT(tmp + 1, in, lzbc);
         in += lzbc;
         // DECODE VALUE #2
         tmp[1] ^= (header & 8) ? dfcm_prediction : fcm_prediction;
@@ -540,7 +603,7 @@ FPC_ATTR void FPC_CALL fpc32_decode_explicit(
         dfcm_prediction = ctx->dfcm[dfcm_hash] + tmp[1];
         prior_value = tmp[1];
         // STORE DECOMPRESSED PAIR
-        (void)memcpy(out_values - 1, tmp, 8);
+        FPC_COPY_FLOAT2(out_values - 1, tmp);
         ++out_values;
     }
 }
@@ -549,18 +612,18 @@ FPC_ATTR size_t FPC_CALL fpc32_encode(
     fpc32_context_t* ctx,
     const float* values,
     size_t value_count,
-    uint8_t* out_compressed)
+    void* out_compressed)
 {
-    return fpc32_encode_explicit(ctx, values, value_count, out_compressed, out_compressed + FPC32_UPPER_BOUND_METADATA(value_count));
+    return fpc32_encode_explicit(ctx, values, value_count, out_compressed, (uint8_t*)out_compressed + FPC32_UPPER_BOUND_METADATA(value_count));
 }
 
 FPC_ATTR void FPC_CALL fpc32_decode(
     fpc32_context_t* ctx,
-    const uint8_t* compressed,
+    const void* compressed,
     float* out_values,
     size_t out_count)
 {
-    fpc32_decode_explicit(ctx, compressed, compressed + FPC32_UPPER_BOUND_METADATA(out_count), out_values, out_count);
+    fpc32_decode_explicit(ctx, compressed, (uint8_t*)compressed + FPC32_UPPER_BOUND_METADATA(out_count), out_values, out_count);
 }
 FPC_EXTERN_C_END
 #endif
